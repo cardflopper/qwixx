1308393316d84a7b2490f93f7bdd3f4e22dca369
cardflopper <smchacko@gmail.com>
2019-01-02 15:08:48 -0500

    cleaned up functions

--- src.js
+++ src.js
@@ -1,17 +1,14 @@
-let colors = ["blue", "green", "red", "yellow"];
-
 /**
-- create an array of dice values, one element for each color (enumerated in the "colors" array)
+- generate dice values object containing "color" : 1d6
 - call the createcombinations function to get combos to display on page
 - update the page
 */
 
 function roll() {
+  let colors = ["blue", "green", "red", "yellow"];
   let diceValues = {};
   for (let color of colors) diceValues[color] = rollDie();
-  //console.log(diceValues);
   let combos = createCombinations(diceValues);
-  //console.log(combos);
   updatePageElements(combos);
   return;
 }
@@ -31,19 +28,18 @@ function createCombinations(diceValues) {
   //console.log("w1:" + white1 + ", w2:" + white2);
 
   let combinations = {};
-  for (let color of colors) {
+
+  for (let color of Object.keys(diceValues)) {
     combinations[color] =
       white1 + diceValues[color] + " / " + (white2 + diceValues[color]);
   }
 
-  combinations["white"] = white1 + white2;
+  combinations["white"] = `${white1 + white2}`;
   return combinations;
 }
 
 function updatePageElements(combos) {
-  let tmpColors = colors;
-  tmpColors.push("white");
-  for (let color of tmpColors)
+  for (let color in combos)
     document.getElementById(color).innerHTML = combos[color];
   return;
 }
